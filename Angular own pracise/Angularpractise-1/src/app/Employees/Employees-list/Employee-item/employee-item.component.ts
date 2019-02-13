ec40c215036cfe313e450957fafbb6a100d1624d
Sunil Kumar <sunilkumar.mandadi@gmail.com>
2019-02-12 17:10:30 -0800

    git commit

--- Angular own pracise/Angularpractise-1/src/app/Employees/Employees-list/Employee-item/employee-item.component.ts
+++ Angular own pracise/Angularpractise-1/src/app/Employees/Employees-list/Employee-item/employee-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Employees } from '../../emp.model';
+import { DataService } from 'src/app/data.service';
 
 
 
@@ -12,13 +13,13 @@ export class EmployeeItemComponent implements OnInit {
   @Input() employee: Employees; //// this is an input for employee-list.componet.html
   @Input() index: number;
  // @Output() employeeselected = new EventEmitter<void>(); /// we can listen this event from outside(means in employee-list.component.html)
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
     // const ss = this.employee;
   }
   onSelected() {
-
+this.dataService.employeeSelected.emit(this.employee);
   }
 
 }
